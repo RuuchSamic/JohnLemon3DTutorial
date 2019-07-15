930bc0247822d22440809078b89c4358ff92d28e
Marcus Chiu <marcus.s.chiu@gmail.com>
2019-07-14 18:14:20 -0700

    Added background music, win music, lose music, and John Lemon footstop audio. Change AudioListener from Main Camera to John Lemon

--- JohnLemon3DTutorial/Assets/Scripts/GameEnding.cs
+++ JohnLemon3DTutorial/Assets/Scripts/GameEnding.cs
@@ -10,9 +10,12 @@ public class GameEnding : MonoBehaviour
     public GameObject player;
     public CanvasGroup exitBackgroundImageCanvasGroup;
     public CanvasGroup caughtBackgroundImageCanvasGroup;
+    public AudioSource exitAudio;
+    public AudioSource caughtAudio;
 
     private bool m_IsPlayerAtExit;
     private bool m_IsPlayerCaught;
+    private bool m_HasAudioPlayed;
     private float m_Timer;
 
     private void OnTriggerEnter(Collider other)
@@ -32,16 +35,22 @@ public class GameEnding : MonoBehaviour
     {
         if (m_IsPlayerAtExit)
         {
-            EndLevel(exitBackgroundImageCanvasGroup, false);
+            EndLevel(exitBackgroundImageCanvasGroup, false, exitAudio);
         }
         else if (m_IsPlayerCaught)
         {
-            EndLevel(caughtBackgroundImageCanvasGroup, true);
+            EndLevel(caughtBackgroundImageCanvasGroup, true, caughtAudio);
         }
     }
 
-    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart)
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart, AudioSource audioSource)
     {
+        if (!m_HasAudioPlayed)
+        {
+            audioSource.Play();
+            m_HasAudioPlayed = true;
+        }
+
         m_Timer += Time.deltaTime;
         imageCanvasGroup.alpha = m_Timer / fadeDuration;
 
