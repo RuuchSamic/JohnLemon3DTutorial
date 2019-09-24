fb51bb08de36baf7a98c67990fa43181c442df39
Marcus Chiu <marcus.s.chiu@gmail.com>
2019-09-24 16:21:32 -0700

    Created Trigger area for player to attack enemy. Started on player's attack script including error messages if enemy is too far or if player is not facing the enemy.

---
+++ JohnLemon3DTutorial/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+
+    public Transform player;
+    public Transform enemy;
+    //public EnemyStats enemyStats;
+    public StopWatch attackInterval;
+    public StopWatch messageInterval;
+
+    private int damage = 25;
+    private float timeToNextAttack = 4.0f;
+    private float playerToEnemyMaximumDistance = 1.0f;
+    private float playerToEnemyActualDistance;
+    private float errorMessageInterval = 5.0f;
+    private bool m_IsEnemyInAttackArea = false;
+    private bool attackMode = false;
+    private string tooFar = "You are too far away!";
+    private string faceEnemy = "Enemy must be in front of you!";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (attackMode)
+        {
+            playerToEnemyActualDistance = Vector3.Distance(player.position, enemy.position);
+
+            if (playerToEnemyActualDistance > playerToEnemyMaximumDistance && messageInterval.repeatTime(errorMessageInterval))
+            {
+                Debug.Log(tooFar);
+            }
+
+            else if (!m_IsEnemyInAttackArea && messageInterval.repeatTime(errorMessageInterval))
+            {
+                Debug.Log(faceEnemy);
+            }
+
+            else
+            {
+                //Do DMG over time
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform == player)
+        {
+            m_IsEnemyInAttackArea = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform == player)
+        {
+            m_IsEnemyInAttackArea = false;
+        }
+    }
+}
